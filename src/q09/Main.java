8c16b854e2168d4e1bdfef362b5f9ed3a986ffb3
Rishita11-Mishra <r29351526@gmail.com>
2026-04-07 18:24:28 +0530

    Update Main.java

--- src/q09/Main.java
+++ src/q09/Main.java
@@ -1,31 +1,21 @@
-
- package.q09
-     import java.util.*;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         int[][] A = new int[n][n];
         int[][] B = new int[n][n];
         int[][] C = new int[n][n];
-
-        // Read matrix A
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 A[i][j] = sc.nextInt();
             }
         }
-
-        // Read matrix B
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 B[i][j] = sc.nextInt();
             }
         }
-
-        // Multiply A and B → C
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 C[i][j] = 0;
@@ -34,8 +24,6 @@ public class Main {
                 }
             }
         }
-
-        // Print result matrix C
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(C[i][j]);
