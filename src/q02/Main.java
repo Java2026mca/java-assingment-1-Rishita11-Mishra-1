d769eba050285cd1e32a2e22048175572efe6356
Rishita11-Mishra <r29351526@gmail.com>
2026-04-07 17:50:12 +0530

    Update Main.java

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,22 +1,15 @@
  import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         for (int i = 0; i < n; i++) {
-            int num = 1; // First value in every row is always 1
-
+            int num = 1; 
             for (int j = 0; j <= i; j++) {
                 System.out.print(num);
-
-                // Avoid trailing space
                 if (j < i) {
                     System.out.print(" ");
                 }
-
-                // Calculate next value using formula
                 num = num * (i - j) / (j + 1);
             }
             System.out.println();
